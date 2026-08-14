073e32437c97297530a56cf478faf55d9840e094
Konstantin S. <havendv@gmail.com>
2026-08-14 19:45:02 +0400

    feat: Updated OpenAPI spec (#18)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Sloyd/Generated/Sloyd.SloydClient.g.cs
+++ src/libs/Sloyd/Generated/Sloyd.SloydClient.g.cs
@@ -38,10 +38,17 @@ namespace Sloyd
 
         /// <inheritdoc/>
         public global::Sloyd.AutoSDKClientOptions Options { get; }
+
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Sloyd.SourceGenerationContext.Default);
+
         /// <summary>
         /// 
         /// </summary>
-        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext { get; set; } = global::Sloyd.SourceGenerationContext.Default;
+        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext
+        {
+            get => JsonSerializerContextProvider.Value;
+            set => JsonSerializerContextProvider = new(() => value);
+        }
 
 
         /// <summary>
