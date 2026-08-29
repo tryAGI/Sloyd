998cf7d8a603de709657322696d27cea8448e255
Konstantin S. <havendv@gmail.com>
2026-08-29 09:12:42 +0400

    feat: Updated OpenAPI spec (#22)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Sloyd/Generated/Sloyd.JsonConverters.EditObjectRequest.g.cs
+++ src/libs/Sloyd/Generated/Sloyd.JsonConverters.EditObjectRequest.g.cs
@@ -17,104 +17,34 @@ namespace Sloyd.JsonConverters
 
             using var __jsonDocument = global::System.Text.Json.JsonDocument.ParseValue(ref reader);
             var __rawJson = __jsonDocument.RootElement.GetRawText();
-            var __jsonProps = new global::System.Collections.Generic.HashSet<string>();
-            if (__jsonDocument.RootElement.ValueKind == global::System.Text.Json.JsonValueKind.Object)
-            {
-                foreach (var __jsonProp in __jsonDocument.RootElement.EnumerateObject())
-                {
-                    __jsonProps.Add(__jsonProp.Name);
-
-                }
-            }
-
-            var __score0 = 0;
-            if (__jsonProps.Contains("AiPromptModifiers")) __score0++;
-            if (__jsonProps.Contains("ClientId")) __score0++;
-            if (__jsonProps.Contains("ClientSecret")) __score0++;
-            if (__jsonProps.Contains("LOD")) __score0++;
-            if (__jsonProps.Contains("ModelOutputType")) __score0++;
-            if (__jsonProps.Contains("Prompt")) __score0++;
-            if (__jsonProps.Contains("ResponseEncoding")) __score0++;
-            if (__jsonProps.Contains("ThumbnailPreviewExportType")) __score0++;
-            if (__jsonProps.Contains("ThumbnailPreviewSizeX")) __score0++;
-            if (__jsonProps.Contains("ThumbnailPreviewSizeY")) __score0++;
-            var __score1 = 0;
-            if (__jsonProps.Contains("InteractionId")) __score1++;
-            var __bestScore = 0;
-            var __bestIndex = -1;
-            if (__score0 > __bestScore) { __bestScore = __score0; __bestIndex = 0; }
-            if (__score1 > __bestScore) { __bestScore = __score1; __bestIndex = 1; }
 
             global::Sloyd.CreateObjectRequest? create = default;
+            try
+            {
+                var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Sloyd.CreateObjectRequest), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Sloyd.CreateObjectRequest> ??
+                               throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Sloyd.CreateObjectRequest).Name}");
+                create = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
+            }
+            catch (global::System.Text.Json.JsonException)
+            {
+            }
+            catch (global::System.InvalidOperationException)
+            {
+            }
+
             global::Sloyd.EditObjectRequestVariant2? editObjectRequestVariant2 = default;
-            if (__bestIndex >= 0)
+            try
             {
-                if (__bestIndex == 0)
-                {
-                    try
-                    {
-                        var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Sloyd.CreateObjectRequest), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Sloyd.CreateObjectRequest> ??
-                                       throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Sloyd.CreateObjectRequest).Name}");
-                        create = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
-                    }
-                    catch (global::System.Text.Json.JsonException)
-                    {
-                    }
-                    catch (global::System.InvalidOperationException)
-                    {
-                    }
-                }
-                else if (__bestIndex == 1)
-                {
-                    try
-                    {
-                        var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Sloyd.EditObjectRequestVariant2), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Sloyd.EditObjectRequestVariant2> ??
-                                       throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Sloyd.EditObjectRequestVariant2).Name}");
-                        editObjectRequestVariant2 = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
-                    }
-                    catch (global::System.Text.Json.JsonException)
-                    {
-                    }
-                    catch (global::System.InvalidOperationException)
-                    {
-                    }
-                }
+                var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Sloyd.EditObjectRequestVariant2), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Sloyd.EditObjectRequestVariant2> ??
+                               throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Sloyd.EditObjectRequestVariant2).Name}");
+                editObjectRequestVariant2 = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
             }
-
-            if (create == null && editObjectRequestVariant2 == null)
+            catch (global::System.Text.Json.JsonException)
             {
-                try
-                {
-
-                    var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Sloyd.CreateObjectRequest), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Sloyd.CreateObjectRequest> ??
-                                   throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Sloyd.CreateObjectRequest).Name}");
-                    create = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
-                }
-                catch (global::System.Text.Json.JsonException)
-                {
-                }
-                catch (global::System.InvalidOperationException)
-                {
-                }
             }
-
-            if (create == null && editObjectRequestVariant2 == null)
+            catch (global::System.InvalidOperationException)
             {
-                try
-                {
-
-                    var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Sloyd.EditObjectRequestVariant2), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Sloyd.EditObjectRequestVariant2> ??
-                                   throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Sloyd.EditObjectRequestVariant2).Name}");
-                    editObjectRequestVariant2 = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
-                }
-                catch (global::System.Text.Json.JsonException)
-                {
-                }
-                catch (global::System.InvalidOperationException)
-                {
-                }
             }
-
             var __value = new global::Sloyd.EditObjectRequest(
                 create,
 
@@ -133,18 +63,46 @@ namespace Sloyd.JsonConverters
             options = options ?? throw new global::System.ArgumentNullException(nameof(options));
             var typeInfoResolver = options.TypeInfoResolver ?? throw new global::System.InvalidOperationException("TypeInfoResolver is not set.");
 
+
+            writer.WriteStartObject();
+            var __writtenPropertyNames = new global::System.Collections.Generic.HashSet<string>(global::System.StringComparer.Ordinal);
             if (value.IsCreate)
             {
                 var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Sloyd.CreateObjectRequest), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Sloyd.CreateObjectRequest?> ??
                                throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Sloyd.CreateObjectRequest).Name}");
-                global::System.Text.Json.JsonSerializer.Serialize(writer, value.Create!, typeInfo);
+                var __element0 = global::System.Text.Json.JsonSerializer.SerializeToElement(value.Create!, typeInfo);
+                if (__element0.ValueKind != global::System.Text.Json.JsonValueKind.Object)
+                {
+                    throw new global::System.Text.Json.JsonException("AllOf values must serialize as JSON objects.");
+                }
+
+                foreach (var __property in __element0.EnumerateObject())
+                {
+                    if (__writtenPropertyNames.Add(__property.Name))
+                    {
+                        __property.WriteTo(writer);
+                    }
+                }
             }
-            else if (value.IsEditObjectRequestVariant2)
+            if (value.IsEditObjectRequestVariant2)
             {
                 var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Sloyd.EditObjectRequestVariant2), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Sloyd.EditObjectRequestVariant2?> ??
                                throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Sloyd.EditObjectRequestVariant2).Name}");
-                global::System.Text.Json.JsonSerializer.Serialize(writer, value.EditObjectRequestVariant2!, typeInfo);
+                var __element1 = global::System.Text.Json.JsonSerializer.SerializeToElement(value.EditObjectRequestVariant2!, typeInfo);
+                if (__element1.ValueKind != global::System.Text.Json.JsonValueKind.Object)
+                {
+                    throw new global::System.Text.Json.JsonException("AllOf values must serialize as JSON objects.");
+                }
+
+                foreach (var __property in __element1.EnumerateObject())
+                {
+                    if (__writtenPropertyNames.Add(__property.Name))
+                    {
+                        __property.WriteTo(writer);
+                    }
+                }
             }
+            writer.WriteEndObject();
         }
     }
 }
